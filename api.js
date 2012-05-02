75d3a8a80ce8d8ce85be2f6f32a64d0308cae0c6
Rob Huzzey <github-commits@robhuzzey.co.uk>
2012-05-02 22:46:00 +0100

    Total refactor of API code, much simpler :)

--- api.js
+++ api.js
@@ -1,20 +1,3 @@
-/*
-	I need help here... 
-	
-	This works for the following urls (assuming you are running node in localhost:5000)
-	http://localhost:5000/delicious/v2/json/djhuzz?count=100
-	http://localhost:5000/google/books/v1/users/101891936560271534706/bookshelves/4/volumes?maxResults=40
-	
-	But NOT on:
-	http://localhost:5000/twitter/1/statuses/user_timeline.json?screen_name=theHuzz
-	
-	I just get a 'not authorised' message....
-	
-	If you can figure this out, I would be very happy :)
-
-*/
-
-
 // Required packages / files
 var private = require( './private/private.js' ),
 	express = require( 'express' ),
@@ -39,152 +22,104 @@ var apis = {
 	}
 };
 
+// Hold the collection in mongoDB I am using
+var collection = 'robhuzzey';
 
-// Hold everything in a nice, easy to manage application object :)
-var App = function() {
+// Simple cache checker function
+var cacheExpired = function( obj ) {
+
+	// Hold the lastmodified date that could be created date if new item
+	var lastModified = obj.modified ? obj.modified : obj.created;
 	
-	return {
+	// Setup the expiry date
+	var expires = lastModified;
+	expires.setHours( expires.getHours() + 1 ); // 1 hour expiry
+	console.log( 'Expiry Date: ' + expires );
 	
-		init : function( params ) {
-			this.params = params;
-			this.setConnection( params.db );
-		},
-		
-		output : function( data ) {
-			this.params.server.response.send( data );
-		},
-	
-		setConnection : function( db ) {
-			// Set connection to app if we have one
-			this._db = db.conn || false;
-			// Log the erro if we have one
-			if( db.err ) {
-				console.log( db.err );
-			}
-		},	
-		
-		getConnection : function() {
-			return this._db;
-		},
-		
-		getCache : function( params ) {
-			
-			var self = this;
-			
-			var conn = this.getConnection();
-			
-			if( conn ) {
-				// get the record from the collection that matches our hash
-				conn.collection( this.params.db.collection, function( err, coll ) {
-					coll.findOne( { 'hash' : params.hash }, function( err, result ) {
-					
-						if( self.cacheExpired( result ) || err ) {
-							params.callback( false );
-							console.log( ( err ? 'Error connecting to cache' : 'Cache expired' ) );
-						} else {
-							
-							data = result;
-							params.callback( data );
-							console.log( 'got from cache' );
-						
-						}
-						
-					});
-				});
-			} else {
-				params.callback( false );
-				console.log( 'failed to get cache' );
-			}
-			
-		},
-		
-		// TODO: Rather than return false, decided on whether the cache has expired using
-		// the mongoDB id as the cache creation date.
-		cacheExpired : function( obj ) {
-			
-			// default our return variable to true (shows we don't have a cache stored yet)
-			var ret = true;
-			
-			// If we don't have obj or obj doesn't have an _id
-			// then it hasn't been saved yet so let's set our return
-			// var to true here.
-			// Additional option to just switch cache off!
-			if( this.params.noCache || !obj || !obj._id ) {
-				ret = true;
-			} else {
-			
-				// Check the timestamp, if it's inside our cache expiry time
-				// then set the object to data
-				var timestamp = obj._id.toString().substring( 0, 8 );
-				var date = new Date( parseInt( timestamp, 16 ) * 1000 );
-				console.log( 'Cache Date: ' + date );
-				ret = false;
-			}
-			
-			return ret;
-			
-		},
-		
-		getApi : function( callback ) {
-						
-			var url = this.params.server.request.url.match(/^\/(\w*)\/(.*)/);
-			var provider = url[1];
-			var endpoint = url[2];
-			
-			// The options hash for the api request
-			var options = {
-				host: this.params.apis[provider].host,
-				port: this.params.apis[provider].port,
-				path: '/' + endpoint,
-				method: 'GET'
-			}
-			
-			var data = '';
-			
-			// The API request
-			var req = require( this.params.apis[provider].protocol ).request( options, function( res ) {
-				res.setEncoding( 'utf8' );
-				
-				// Build up the response in data
-				res.on( 'data', function( chunk ) {
-					data += chunk;
-				});
-				
-				// Once the api has finished, pass data to the callback
-				res.on( 'end', function() {
-					console.log( 'got from api' );
-					callback( data );
-				});
-			});
-			
-			// End the request on the api
-			req.end();
-			
-		},
-		
-		saveCache : function( params ) {
-		
-			if( this.params.noCache ) {
-				console.log( 'cache turned off' );	
-			} else {
-				var conn = this.getConnection();			
-				if( conn && params.data ) {
-					conn.collection( this.params.db.collection, function( err, coll ) {
-						var object_to_insert = { "hash" : params.hash, "data" : params.data };
-						coll.save( object_to_insert, { safe : true }, function( err ) {
-							console.log( 'saved cache' );
-							//params.callback( object_to_insert );
-						});
-					});
-				}
-			}		
-		}
-		
-	};
+	// Check if our expiry date is after now (returns bool)
+	return ( new Date() > expires );
 	
 };
 
 
+var appHelpers = function( mongoDBconn, collection ) {
+
+	return {
+
+		conn : mongoDBconn,
+		
+		getCache : function( options ) {
+			// If we have a connection, let's try and get the cache, otherswise drop right into erro callback
+			if( this.conn ) {
+				var self = this;
+				self.conn.collection( collection, function( err, coll ) {
+					coll.findOne( { 'hash' : options.hash }, function( err, result ) {
+						if( result ) {
+							// If the record we found has expired, drop into error callback
+							// otherwise success
+							if( self.cacheExpired( result ) ) {
+								options.error( result );
+								console.log( 'uh oh!' );
+							} else {
+								options.success( result );
+							}
+						} else {
+							options.error( result );
+						}
+					});
+				});
+			} else {
+				options.error();
+				console.log( 'Unable to establish connection to mongoDB' );
+			}					
+		},
+		
+		saveCache : function( options ) {
+		
+			if( this.conn ) {
+				// Save the cache
+				this.conn.collection( collection, function( err, coll ) {
+					var object_to_insert = { "hash" : options.hash, "data" : options.data, "created" : null, "modified" : null };
+						
+					// Set the _id to our object_to_insert if we have one
+					if( options.mongoObj && options.mongoObj._id ) {
+						object_to_insert['_id'] = options.mongoObj._id;
+						object_to_insert['modified'] = new Date();
+					} else {
+						object_to_insert['created'] = new Date();
+					}
+					
+					// Now save / update the object
+					coll.save( object_to_insert, { safe : true }, function( err ) {
+						console.log( 'saved cache' );
+					});								
+					
+				});
+			} else {
+				console.log( 'Unable to establish connection to mongoDB' );
+			}
+		
+		},
+		
+		cacheExpired : function( obj ) {
+			// Hold the lastmodified date that could be created date if new item
+			var lastModified = obj.modified ? obj.modified : obj.created;
+			
+			// Setup the expiry date
+			var expires = lastModified;
+			expires.setHours( expires.getHours() + 1 ); // 1 hour expiry
+			console.log( 'Expiry Date: ' + expires );
+			
+			var now = new Date();
+			console.log( 'Now: ' + now );
+			
+			// Check if our expiry date is after now (returns bool)
+			return ( now > expires );
+		}
+
+	};
+	
+};
 
 
 // Start off by defining the server
@@ -193,72 +128,81 @@ var server = express.createServer( express.logger() );
 // Initialize the server
 server.get( '*' , function( request, response ) {
 
+	// Quick fix to prevent the .ico file triggering this code
 	if( request.params[0] != '/favicon.ico' ) {
-
-		var app = new App();
-	
 	
 		// Make a unique identifier for the request
-		var hash = require( 'crypto' ).createHash( 'md5' ).update( request.url ).digest( 'hex' );
-	
-	
-		// Wrap everything in our database connection
+		var hash = require( 'crypto' ).createHash( 'md5' ).update( request.url ).digest( 'hex' );	
+		
+		// First up, see if we have a cache of this request already
 		mongodb.connect( private.mongolab.uri, function( err, conn ) {
 			
-			// initialise the application		
-			app.init({
-				"db" : {
-					"conn" : conn,
-					"error" : err,
-					"collection" : "robhuzzey"
-				},
-				"server" : {
-					"request" : request,
-					"response" : response
-				},
-				"apis" : apis,
-				
-				"noCache" : true
-			});
-			
-			// Start by trying to get the cache
-			app.getCache({
+			// Init a new helper and try to get the cache
+			var helper = new appHelpers( conn, collection );
+			helper.getCache({
 				"hash" : hash,
-				"callback" : function( cache ) {
+				"success" : function( data ) {
+					console.log( 'got from cache' );
+					response.send( data.data );
+				},
+				"error" : function( mongoObj ) {
 					
-					if( cache && cache.data ) {
-						// If we have data from the cache, send it to the output
-						app.output( cache.data );
-					} else {
+					// Break down the request url into parts we need	
+					var url = request.url.match(/^\/(\w*)\/(.*)/);
+					var provider = url[1];
+					var endpoint = url[2];
 					
-						// if we don't have data from cache, lookup in the API and send that on
-						// saving to the cache again
-						app.getApi( function( data ) {
+					// The options hash for the api request
+					var options = {
+						host: apis[provider].host,
+						port: apis[provider].port,
+						path: '/' + endpoint,
+						method: 'GET'
+					}
+					
+					var data = '';
+					
+					// The API request
+					var req = require( apis[provider].protocol ).request( options, function( res ) {
+						res.setEncoding( 'utf8' );
+						
+						// Build up the response in data
+						res.on( 'data', function( chunk ) {
+							data += chunk;
+						});
+						
+						// Once the api has finished, send output & save the cache
+						res.on( 'end', function() {
+							console.log( 'got from api' );
+							response.send( data );
 							
-							// Send output
-							app.output( data );
-							
-							// Save cache
-							app.saveCache({
+							helper.saveCache({
 								"hash" : hash,
-								"data" : data
+								"data" : data,
+								"mongoObj" : mongoObj
 							});
 							
 						});
-						
-					}
+					});
+					
+					// End the request on the api
+					req.end();			
+				
 				}
+				
 			});
+				
+				
 			
 		});
-
+			
+	
 	}
 	
 });
 
 
-
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 server.listen( port, function() {
 	console.log( "Listening on " + port );
 });
